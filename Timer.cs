90d77d38d533aad5a05c040daa19bf969ed91e46
matxcs <matxcs@users.noreply.github.com>
2026-06-29 17:36:32 -0500

    Continuing timer implementation, researching current/elapsed time techniques

---
+++ Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http.Headers;
+using System.Windows.Threading;
+
+namespace Pomodoro
+{
+    public class Timer
+    {
+        DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+        public Timer() { Duration = 0;}
+        private uint _duration;
+        private uint _remaining;
+        public uint Duration { 
+            get => _duration;
+            set
+            {
+                if (value < 0) { value = 0; }
+                _duration = value;
+            }
+        }
+
+        public uint Remaining
+        {
+            get => _remaining;
+            set
+            {
+                if (value < 0) { value = 0; }
+                _remaining = value;
+            }
+        }
+        public void Reset() => Remaining = Duration;
+        public void Start(uint duration) {
+            Duration = duration;
+            Remaining = duration;
+        }
+        public void Stop() { }
+    }
+}
